8dd8e65a6000dae88bbcaf67ee98476570bf5e32
lucmarc02 <danilucascoe@gmail.com>
2025-11-18 20:38:46 -0300

    siteinfo-reacts

---
+++ React/siteinfo-reactfacts/src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <h1 className="main-title">Razões para aprender React</h1>
+      <ul className="main-facts">
+        <li>Tem uma comunidade muito grande!</li>
+        <li>É mais fácil conseguir emprego</li>
+        <li>É de fácil aprendizado</li>
+        <li>Muito usado nas empresas</li>
+        <li>Tem mais de 100K estrelas no Github</li>
+      </ul>
+    </main>
+  );
+}
---
+++ React/siteinfo-reactfacts/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="./images/logo-react.png"
+        className="nav-icon"
+        alt="logo-react"
+      />
+      <h3 className="nav-logo_text">ReactFacts</h3>
+      <h4 className="nav-title">React Course - Project 1</h4>
+    </nav>
+  );
+}
